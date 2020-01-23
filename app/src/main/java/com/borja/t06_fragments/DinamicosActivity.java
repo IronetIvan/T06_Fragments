e5d11c69c156da5c7e22cff9005a56c5b912ca5a
Iván Redondo <ivan.redondo@cesjuanpablosegundo.es>
2020-01-23 11:57:22 +0100

    act DinamicosActivity ERROR

--- app/src/main/java/com/borja/t06_fragments/DinamicosActivity.java
+++ app/src/main/java/com/borja/t06_fragments/DinamicosActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.borja.t06_fragments.fragments.FragmentDinamicoDos;
+import com.borja.t06_fragments.fragments.FragmentDinamicoUno;
 import com.borja.t06_fragments.fragments.FragmentDinamicoUno;
 
 public class DinamicosActivity extends AppCompatActivity implements View.OnClickListener
